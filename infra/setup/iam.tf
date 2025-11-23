dc4a65f20b4798e4cc2478e29adfaec1b20660d7
John Dennehy <johndennehy101@gmail.com>
2025-11-23 11:15:37 +0000

    Add ECR permissions for CI/CD user

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -51,3 +51,41 @@ resource "aws_iam_user_policy_attachment" "terraform_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.terraform_backend.arn
 }
+
+###############################################
+# Define Policy for ECR access for CI/CD user #
+###############################################
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.frontend.arn,
+      aws_ecr_repository.backend.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Enable CI/CD user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
+}
