c43fa8587fe468d7251f04f838500cb932e698c8
John Dennehy <johndennehy101@gmail.com>
2025-11-29 16:06:13 +0000

    Use local storage for Prometheus on cloud

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -1,6 +1,6 @@
-##################################################
-# EFS Security Group for Prometheus data storage #
-##################################################
+######################################
+# EFS Security Group for EFS storage #
+######################################
 
 resource "aws_security_group" "efs" {
   name        = "${local.prefix}-efs"
@@ -24,60 +24,6 @@ resource "aws_security_group" "efs" {
   tags = {
     Name = "${local.prefix}-efs"
   }
-}
-
-#####################################################
-# EFS File System for Prometheus data storage #
-#####################################################
-
-resource "aws_efs_file_system" "prometheus_data" {
-  creation_token = "${local.prefix}-prometheus-data"
-
-  performance_mode                = "generalPurpose"
-  throughput_mode                 = "provisioned"
-  provisioned_throughput_in_mibps = 100
-
-  tags = {
-    Name = "${local.prefix}-prometheus-data"
-  }
-}
-
-################################################
-# EFS Access Point for Prometheus data storage #
-################################################
-
-resource "aws_efs_access_point" "prometheus_data" {
-  file_system_id = aws_efs_file_system.prometheus_data.id
-
-  posix_user {
-    gid = 0
-    uid = 0
-  }
-
-  root_directory {
-    path = "/data"
-    creation_info {
-      owner_gid   = 0
-      owner_uid   = 0
-      permissions = "755"
-    }
-  }
-}
-
-#################################################
-# EFS Mount Targets for Prometheus data storage #
-#################################################
-
-resource "aws_efs_mount_target" "prometheus_data_a" {
-  file_system_id  = aws_efs_file_system.prometheus_data.id
-  subnet_id       = aws_subnet.private_a.id
-  security_groups = [aws_security_group.efs.id]
-}
-
-resource "aws_efs_mount_target" "prometheus_data_b" {
-  file_system_id  = aws_efs_file_system.prometheus_data.id
-  subnet_id       = aws_subnet.private_b.id
-  security_groups = [aws_security_group.efs.id]
 }
 
 #################################################
